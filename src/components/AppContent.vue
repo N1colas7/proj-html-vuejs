a505d2251d054c9ff443d52804c870b17ac89fa3
N1colas7 <nicolasnd9897@gmail.com>
2023-01-24 16:31:22 +0100

    try to do jumbo slider

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -2,17 +2,37 @@
 export default {
     data() {
         return {
-            activeSlide: 0,
+            activeSlide:0,
+            slideJumbo:[
+                {
+                    Image:"../assets/img/home1_slide_2_bg.jpg",
+                    Text:"d",
+                    button:"d",
+                },
+                {
+                    Image:"../assets/img/home1_slide_one_bg.jpg",
+                    Text:"d",
+                    button:"d",
+                },
+                {
+                    Image:"../assets/img/home1_slide_three_bg_2.jpg",
+                    Text:"d",
+                    button:"d",
+                },
+            ]
         }
-    },    
+    }   
 }
 
 </script>
 <template lang="">
-    <div class="slider">
-        
+    <div class="slider-jumbo">
+        <img src="slideJumbo[activeSlide].Image" :alt="slideJumbo[activeSlide].Text">
+        <h3>{{ slideJumbo[activeSlide].Text}}</h3>
     </div>
 </template>
 <style lang="scss">
-    
+    .slider-jumbo{
+        width: 100%;
+    }
 </style>
